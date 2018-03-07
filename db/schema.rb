24f8ba6f3e7655350c27d646dfc2e3f670425c22
Fernando Martins <fernando@MacBook-Pro-de-Fernando.local>
2018-03-07 14:38:01 -0300

    implementing votes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180307163139) do
+ActiveRecord::Schema.define(version: 20180307171950) do
 
   create_table "links", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "url"
@@ -29,5 +29,16 @@ ActiveRecord::Schema.define(version: 20180307163139) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "votes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.bigint "link_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["link_id"], name: "index_votes_on_link_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+  end
+
   add_foreign_key "links", "users"
+  add_foreign_key "votes", "links"
+  add_foreign_key "votes", "users"
 end
